5dcc7776c3d2c8ba5ce8ba85ed82e753ea886192
AukeBoonman <auke.boonman@gmail.com>
2020-07-14 14:45:11 +0200

    exercises tot en met leap year

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+
+// SpeedConverter check
+        //    public static void main(String[] args) {
+        //        // psvm --> gives above code
+        //        long miles = SpeedConverter.toMilesPerHour(10.5);
+        //        System.out.println("Miles = " + miles);
+        //
+        //        SpeedConverter.printConversion(10.5);
+        //    }
+
+// MegaBytes Converter check
+        //    public static void main(String[] args) {
+        //        MegaBytesConverter.printMegaBytesAndKiloBytes(5000);
+        //    }
+
+// Barking Dog check
+        //    public static void main(String[] args) {
+        //
+        //        boolean answer = BarkingDog.shouldWakeUp(false, 7);
+        //        System.out.println(answer);
+        //    }
+
+// Leap Year Calculator
+        //    public static void main(String[] args) {
+        //        boolean leapYear = LeapYearCalculator.isLeapYear(2000);
+        //        System.out.println(leapYear);
+        //    }
+
+
+}
---
+++ src/MegaBytesConverter.java
@@ -0,0 +1,18 @@
+//public class MegaBytesConverter {
+//
+//    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+//
+//        int megabytes;
+//        int remainingKilobytes;
+//
+//        megabytes = (kiloBytes / 1024);
+//        remainingKilobytes = (kiloBytes % 1024);
+//
+//        if (kiloBytes < 0) {
+//            System.out.println("Invalid Value");
+//        } else {
+//            System.out.println(kiloBytes + " KB = " + megabytes + " MB and " + remainingKilobytes + " KB");
+//        }
+//    }
+//
+//}
